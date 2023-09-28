d56f2cbc9d071e88f527848c29008e79ad9921ce
Bruno Britto <bruno_britto10@hotmail.com>
2023-09-28 10:44:28 +0200

    Swaggers configurations added

---
+++ src/main/java/com/educandoweb/course/config/OpenApiConfig.java
@@ -0,0 +1,20 @@
+package com.educandoweb.course.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import io.swagger.v3.oas.models.info.Info;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .components(new Components())
+                .info(new Info()
+                        .title("Web service project")
+                        .description("\"Swagger configuration for Web service project \""));
+    }
+}
